495fcff095e0f8086b41f898f504b20ce1dfd968
Victor Labussiere <victor.labussiere95@gmail.com>
2024-01-02 20:40:33 -0300

    tailwindcss setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-function App () {
-  return <h1 className='text-red-500 px-[32px]'>Hello World</h1>
+function App (): JSX.Element {
+  return <h1 className='text-red-500'>Hello World</h1>
 }
 
 export default App
