f5fb72eec464c7087007c19db3a9288d4148aecf
Victor Labussiere <victor.labussiere95@gmail.com>
2024-01-23 13:01:01 -0300

    App router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-import Event from './pages/Event'
+import { ApolloProvider } from '@apollo/client'
+import Router from './Router'
+import { client } from './lib/apollo'
+import { BrowserRouter } from 'react-router-dom'
 
 export default function App (): JSX.Element {
-  return <Event />
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  )
 }
